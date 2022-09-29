f5b04c9e1e80731da66d6399fa8f67490913df94
Ally Murray <allymurray88@gmail.com>
2022-09-29 14:01:33 +0100

    add base resource

---
+++ src/resources/base-resource.test.ts
@@ -0,0 +1,112 @@
+import nock from 'nock';
+import cloneDeep from 'clone-deep';
+import { HttpClientFactory } from '../http-client';
+import { Issue } from './issue/issue';
+import apiRetrieveResponse from '../__mocks__/api-response/issue-details.json';
+import expectedRetrieveResult from '../__mocks__/expected-responses/issue-details.json';
+import apiListResponse from '../__mocks__/api-response/issue-list-item.json';
+import expectedListResult from '../__mocks__/expected-responses/issue-list-item.json';
+
+// TODO: RenameExpected response from issue list item to issue list
+// TODO: Generate tests for all resources
+
+const baseUrl = 'https://comicvine.gamespot.com/api';
+
+const pickProperties = <T>(obj: T, properties: Array<keyof T>) => {
+  return Object.assign(
+    {},
+    ...properties.map((prop) => ({ [prop]: obj[prop] }))
+  );
+};
+
+describe('Issue', () => {
+  const MOCK_SUCCESSFUL_ISSUE_ID = 1;
+  const httpClient = HttpClientFactory.createClient();
+  const urlBuilder = HttpClientFactory.createUrlBuilder('mock-api-key');
+
+  describe('retrieve', () => {
+    it('should return the full resource when a successful request is made', async () => {
+      // Arrange
+      nock(baseUrl)
+        .get('/issue/4000-1?format=json&api_key=mock-api-key')
+        .reply(200, apiRetrieveResponse);
+      const issue = new Issue(httpClient, urlBuilder);
+
+      // Act
+      const response = await issue.retrieve(MOCK_SUCCESSFUL_ISSUE_ID);
+
+      // Assert
+      expect(response).toStrictEqual(expectedRetrieveResult);
+    });
+
+    it('should return the correct properties when the field list is specified', async () => {
+      // Arrange
+      const expectedResult = pickProperties(expectedRetrieveResult, [
+        'id',
+        'name',
+      ]);
+      const mockApiResponse = {
+        ...cloneDeep(apiRetrieveResponse),
+        results: pickProperties(apiRetrieveResponse.results, ['id', 'name']),
+      };
+      nock(baseUrl)
+        .get(
+          '/issue/4000-1?format=json&api_key=mock-api-key&field_list=id%2Cname'
+        )
+        .reply(200, mockApiResponse);
+      const issue = new Issue(httpClient, urlBuilder);
+
+      // Act
+      const response = await issue.retrieve(MOCK_SUCCESSFUL_ISSUE_ID, {
+        fieldList: ['id', 'name'],
+      });
+
+      // Assert
+      expect(response).toStrictEqual(expectedResult);
+    });
+  });
+
+  describe('list', () => {
+    it('should return pagination details and a list of issues when a successful request is made', async () => {
+      // Arrange
+      nock(baseUrl)
+        .get('/issues?format=json&api_key=mock-api-key')
+        .reply(200, apiListResponse);
+      const issue = new Issue(httpClient, urlBuilder);
+
+      // Act
+      const response = await issue.list();
+
+      // Assert
+      expect(response).toStrictEqual(expectedListResult);
+    });
+
+    it('should return the correct properties when the field list is specified', async () => {
+      // Arrange
+      const expectedResult = {
+        ...cloneDeep(expectedListResult),
+        data: apiListResponse.results.map((item) =>
+          pickProperties(item, ['id', 'name'])
+        ),
+      };
+      const mockApiResponse = {
+        ...cloneDeep(apiListResponse),
+        results: apiListResponse.results.map((item) =>
+          pickProperties(item, ['id', 'name'])
+        ),
+      };
+      nock(baseUrl)
+        .get('/issues?format=json&api_key=mock-api-key&field_list=id%2Cname')
+        .reply(200, mockApiResponse);
+      const issue = new Issue(httpClient, urlBuilder);
+
+      // Act
+      const response = await issue.list({
+        fieldList: ['id', 'name'],
+      });
+
+      // Assert
+      expect(response).toStrictEqual(expectedResult);
+    });
+  });
+});
---
+++ src/resources/base-resource.ts
@@ -0,0 +1,45 @@
+import { ResourceType } from './resource-type';
+import { RetrieveOptions, ListOptions, HttpClient, UrlBuilder } from '../types';
+
+export abstract class BaseResource<Resource, ResourceListItem> {
+  protected abstract resourceType: ResourceType;
+
+  constructor(private httpClient: HttpClient, private urlBuilder: UrlBuilder) {}
+
+  async retrieve<FieldKey extends keyof Resource>(
+    id: number,
+    options?: RetrieveOptions<FieldKey>
+  ) {
+    type ReturnType<T> = T extends object ? Pick<Resource, FieldKey> : Resource;
+
+    const url = this.urlBuilder.retrieve(this.resourceType, id, options);
+    const fieldList = options?.fieldList;
+    const response = await this.httpClient.get<ReturnType<typeof fieldList>>(
+      url
+    );
+
+    return response.results;
+  }
+
+  async list<FieldKey extends keyof ResourceListItem>(
+    options?: ListOptions<FieldKey, PickFilters<ResourceListItem>>
+  ) {
+    type ReturnType<T> = T extends object
+      ? Pick<ResourceListItem, FieldKey>
+      : ResourceListItem;
+
+    const url = this.urlBuilder.list(this.resourceType, options);
+    const fieldList = options?.fieldList;
+    const response = await this.httpClient.get<ReturnType<typeof fieldList>[]>(
+      url
+    );
+
+    return {
+      limit: response.limit,
+      numberOfPageResults: response.numberOfPageResults,
+      numberOfTotalResults: response.numberOfTotalResults,
+      offset: response.offset,
+      data: response.results,
+    };
+  }
+}
