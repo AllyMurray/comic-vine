1d77c13a51cb4c429f50fb94c931893e99514c7f
Ally Murray <allymurray88@gmail.com>
2022-09-29 14:01:23 +0100

    add resource factory

---
+++ src/resources/resource-factory.test.ts
@@ -0,0 +1,50 @@
+import type { ValueOf } from '../types';
+
+import { HttpClientFactory } from '../http-client';
+import { ResourceFactory } from './resource-factory';
+import * as resources from './resource-list';
+
+describe('ResourceFactory', () => {
+  const httpClient = HttpClientFactory.createClient();
+  const urlBuilder = HttpClientFactory.createUrlBuilder('mock-api-key');
+  const resourceFactory = new ResourceFactory(httpClient, urlBuilder);
+
+  type FactoryInput = keyof typeof resources;
+  type ResourceClass = ValueOf<typeof resources>;
+  const resourceList: Array<[FactoryInput, ResourceClass]> = [
+    ['Character', resources.Character],
+    ['Concept', resources.Concept],
+    ['Episode', resources.Episode],
+    ['Issue', resources.Issue],
+    ['Location', resources.Location],
+    ['Movie', resources.Movie],
+    ['Origin', resources.Origin],
+    ['Person', resources.Person],
+    ['Power', resources.Power],
+    ['Promo', resources.Promo],
+    ['Publisher', resources.Publisher],
+    ['Series', resources.Series],
+    ['StoryArc', resources.StoryArc],
+    ['Team', resources.Team],
+    ['Thing', resources.Thing],
+    ['VideoCategory', resources.VideoCategory],
+    ['Video', resources.Video],
+    ['VideoType', resources.VideoType],
+    ['Volume', resources.Volume],
+  ];
+
+  test.each(resourceList)(
+    'should return an instance of %s',
+    (factoryInput, expectedReturnType) => {
+      const resource = resourceFactory.create(factoryInput);
+      expect(resource).toBeInstanceOf(expectedReturnType);
+    }
+  );
+
+  it('should return throw if the resource is not implemented', () => {
+    const notImplementedResourceType = 'Unknown' as any;
+    expect(() => resourceFactory.create(notImplementedResourceType)).toThrow(
+      'Unknown resource not implemented'
+    );
+  });
+});
---
+++ src/resources/resource-factory.ts
@@ -0,0 +1,20 @@
+import * as resources from './resource-list';
+import { HttpClient, UrlBuilder } from '../types';
+
+export class ResourceFactory {
+  private _resources = { ...resources };
+
+  constructor(private httpClient: HttpClient, private urlBuilder: UrlBuilder) {}
+
+  public create<T extends keyof typeof this._resources>(
+    name: T
+  ): InstanceType<typeof this._resources[T]>;
+  public create(
+    name: keyof typeof this._resources
+  ): InstanceType<typeof this._resources[keyof typeof this._resources]> {
+    if (!this._resources[name]) {
+      throw new Error(`${name} resource not implemented`);
+    }
+    return new (this._resources[name] as any)(this.httpClient, this.urlBuilder);
+  }
+}
