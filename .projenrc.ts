7aedb2000685ea16e6c402367b4296984242b9e1
Ally Murray <allymurray88@gmail.com>
2023-09-10 01:23:27 +0100

    refactor: :arrow_up: upgrade projen modules to 0.0.11 and projen to 0.73.15

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,8 +6,6 @@ const project = new TypeScriptNpmPackage({
   name: 'comic-vine',
   packageName: 'comic-vine-sdk',
   description: 'A JS/TS client for the Comic Vine API',
-  authorName: 'Ally Murray',
-  authorEmail: 'allymurray88@gmail.com',
   defaultReleaseBranch: 'main',
   deps: ['axios', 'clone-deep', 'zod'],
   devDeps: [
@@ -21,21 +19,6 @@ const project = new TypeScriptNpmPackage({
     'vitest',
   ],
   minMajorVersion: 1,
-  projenrcTs: true,
-  tsconfig: {
-    compilerOptions: {
-      module: 'Node16',
-      target: 'ES2020',
-      // @ts-expect-error types is missing from compilerOptions
-      types: ['vitest/globals'],
-      skipLibCheck: true,
-    },
-  },
-  gitignore: ['.DS_Store', '*yalc*', 'test-reports'],
-  jest: false,
-  repository: `${repository}.git`,
-  bugsUrl: `${repository}/issues`,
-  homepage: `${repository}#readme`,
   keywords: [
     'comic-metadata',
     'comic-vine-api',
@@ -52,25 +35,16 @@ const project = new TypeScriptNpmPackage({
     'comics',
     'metadata',
   ],
-});
-
-project.npmrc.addConfig('save-exact', 'true');
-
-project.testTask.prependExec('vitest --dir=src', { receiveArgs: true });
-
-project.eslint?.addOverride({
-  files: ['*.test.ts'],
-  rules: {
-    'dot-notation': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-      },
-    ],
+  repository,
+  tsconfig: {
+    compilerOptions: {
+      module: 'Node16',
+      target: 'ES2020',
+      // @ts-expect-error types is missing from compilerOptions
+      types: ['vitest/globals'],
+      skipLibCheck: true,
+    },
   },
 });
 
 project.synth();
-
-// TODO: Sort out version number
